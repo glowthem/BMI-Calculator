eaa96e8392eda15122cbf4fe26cfcb8c5de03a26
glowthem <glowing713@naver.com>
2020-07-24 15:11:47 +0900

    2020-07-24 ➕ add SecondViewController

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -29,7 +29,13 @@ class ViewController: UIViewController {
         let weight = weightSlider.value
         
         let bmi = weight / pow(height, 2)
-        print(bmi)
+//        print(bmi)
+        
+        let secondVC = SecondViewController()
+        
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
     }
 }
 
